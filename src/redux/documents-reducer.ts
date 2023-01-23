527284d95832f4b8cf196eb5fbbbe631b562718f
e-doschechnikova <evadrobakova@gmail.com>
2023-01-23 06:45:15 +0000

    fix: reducer and modal

--- src/redux/documents-reducer.ts
+++ src/redux/documents-reducer.ts
@@ -24,16 +24,19 @@ const setDocumentsAC = (documents: any) => ({
     payload: documents
 })
 
+const getData = async () => {
+    const reqDocFirst = await fetch('http://localhost:3010/documents1')
+    const reqDocSecond = await fetch(`http://localhost:3010/documents2`)
+
+    const dataDocFirst = await reqDocFirst.json()
+    const dataDocSecond = await reqDocSecond.json()
+
+    return [...dataDocFirst, ...dataDocSecond]
+}
+
 export const setDocumentsTC = () => async (dispatch: Dispatch) => {
     try {
-        const reqDocFirst = await fetch('http://localhost:3010/documents1')
-        const reqDocSecond = await fetch(`http://localhost:3010/documents2`)
-
-        const dataDocFirst = await reqDocFirst.json()
-        const dataDocSecond = await reqDocSecond.json()
-
-        const concatData = [...dataDocFirst, ...dataDocSecond]
-
+        const concatData = await getData()
         dispatch(setDocumentsAC(concatData))
     } catch (e) {
     }
@@ -51,14 +54,8 @@ export const cancelProductTC = (idProducts: string) => async (dispatch: Dispatch
             })
         });
         if (req.status === 200) {
-            const reqDocFirst = await fetch('http://localhost:3010/documents1')
-            const reqDocSecond = await fetch(`http://localhost:3010/documents2`)
 
-            const dataDocFirst = await reqDocFirst.json()
-            const dataDocSecond = await reqDocSecond.json()
-
-            const concatData = [...dataDocFirst, ...dataDocSecond]
-
+            const concatData = await getData()
             dispatch(setDocumentsAC(concatData))
         }
     } catch (e) {
